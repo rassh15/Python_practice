589967042fe4a75e2982ec6298adcc6c68d30d66
Rashid <rassh1512@gmail.com>
2022-02-08 15:56:24 +0530

    Adding relation in data models for django

--- Django/book_store/book_outlet/admin.py
+++ Django/book_store/book_outlet/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Book
+from .models import Book, Author
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = { "slug": ("title",)}
     list_filter = ("rating", "title")
+    list_display = ("title", "author")
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
 
 
--- Django/book_store/book_outlet/models.py
+++ Django/book_store/book_outlet/models.py
@@ -5,11 +5,21 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name  = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author = models.CharField(null=True,max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True ,null=False)#Harry Potter 1 _> harry-potter-1
 
