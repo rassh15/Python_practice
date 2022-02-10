50ef44203be99f1828f09fd0fd640c784c608f06
Rashid <rassh1512@gmail.com>
2022-02-10 12:21:41 +0530

    adding more form field

--- Django/Feedback/reveiw/forms.py
+++ Django/Feedback/reveiw/forms.py
@@ -1,5 +1,13 @@
+from distutils.log import error
 from socket import fromshare
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name =  forms.CharField()
+    user_name =  forms.CharField(label="Your Name", max_length=100,error_messages={
+        "required": "Your name must not be empty!",
+        "max_lenth": "Please enter a shorter name"
+    })
+
+    review_text = forms.CharField(label="Your Feedback",widget=forms.Textarea, max_length=200)
+    
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
