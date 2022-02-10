f9d60e0cd1895e16a010ed90dca3c25cdc91942a
Rashid <rassh1512@gmail.com>
2022-02-10 12:40:59 +0530

    Form data into database

--- Django/Feedback/reveiw/views.py
+++ Django/Feedback/reveiw/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from .forms import ReviewForm
-
+from .models import Review
 # Create your views here.
 
 def review(request):
@@ -8,7 +8,12 @@ def review(request):
     if request.method=="POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating = form.cleaned_data['rating'],
+            )
+            review.save()
             return HttpResponseRedirect('/thank-you')
 
     else:
