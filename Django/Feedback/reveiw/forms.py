619bc8a561d31185549b163af0e5cfe43434be15
Rashid <rassh1512@gmail.com>
2022-02-10 12:47:18 +0530

    Model Forms

--- Django/Feedback/reveiw/forms.py
+++ Django/Feedback/reveiw/forms.py
@@ -1,13 +1,21 @@
 from distutils.log import error
 from socket import fromshare
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name =  forms.CharField(label="Your Name", max_length=100,error_messages={
-        "required": "Your name must not be empty!",
-        "max_lenth": "Please enter a shorter name"
-    })
+# class ReviewForm(forms.Form):
+#     user_name =  forms.CharField(label="Your Name", max_length=100,error_messages={
+#         "required": "Your name must not be empty!",
+#         "max_lenth": "Please enter a shorter name"
+#     })
 
-    review_text = forms.CharField(label="Your Feedback",widget=forms.Textarea, max_length=200)
+#     review_text = forms.CharField(label="Your Feedback",widget=forms.Textarea, max_length=200)
     
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__' #or spefify [] to  select specific fields
+        # exclude = 
+        
