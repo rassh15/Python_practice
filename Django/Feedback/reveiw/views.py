51b87e5ba56e7c834a483a036110eb0e729bb79f
Rashid <rassh1512@gmail.com>
2022-02-10 11:21:41 +0530

    Adding validation

--- Django/Feedback/reveiw/views.py
+++ Django/Feedback/reveiw/views.py
@@ -6,8 +6,10 @@ def review(request):
 
     if request.method=="POST":
         entered_username = request.POST['username']
+        if entered_username=="":
+            return render(request, 'reveiw.html',{"has_error":True})
         return HttpResponseRedirect('/thank-you')
-    return render(request, 'reveiw.html')
+    return render(request, 'reveiw.html',{"has_error":False})
 
 def thankyou(request):
     return render(request, 'thank-you.html')    
