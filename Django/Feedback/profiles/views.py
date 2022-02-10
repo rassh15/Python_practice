32f0e7f9ad81920fe7347542176ee5f575c8df42
Rashid <rassh1512@gmail.com>
2022-02-10 19:24:17 +0530

    Working with file upload model db

--- Django/Feedback/profiles/views.py
+++ Django/Feedback/profiles/views.py
@@ -1,7 +1,9 @@
+import profile
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from template.prof_form import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
@@ -22,7 +24,8 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
 
         return render(request, "prof_create_profile.html",{
