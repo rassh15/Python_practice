028a661f4cfdca5bd1fea95f9a5193804b581d4d
Rashid <rassh1512@gmail.com>
2022-02-10 19:12:55 +0530

    Manually doing file upload and save

--- Django/Feedback/profiles/views.py
+++ Django/Feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from template.prof_form import ProfileForm
 
 # Create your views here.
 
@@ -11,8 +12,19 @@ def store_file(file):
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "prof_create_profile.html")
+        form = ProfileForm()
+
+        return render(request, "prof_create_profile.html",{
+            "form":form
+        })
 
     def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            store_file(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "prof_create_profile.html",{
+            "form":submitted_form
+        })
