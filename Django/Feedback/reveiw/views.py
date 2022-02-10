f1c3398c733d99267f3fda8850cb7a3bbd1256c2
Rashid <rassh1512@gmail.com>
2022-02-10 11:40:47 +0530

    Django form class

--- Django/Feedback/reveiw/views.py
+++ Django/Feedback/reveiw/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, HttpResponseRedirect
+from .forms import ReviewForm
 
 # Create your views here.
 
 def review(request):
 
     if request.method=="POST":
-        entered_username = request.POST['username']
-        if entered_username=="":
-            return render(request, 'reveiw.html',{"has_error":True})
-        return HttpResponseRedirect('/thank-you')
-    return render(request, 'reveiw.html',{"has_error":False})
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/thank-you')
+
+    else:
+        form = ReviewForm()
+    return render(request, 'reveiw.html',{'form':form})
 
 def thankyou(request):
     return render(request, 'thank-you.html')    
