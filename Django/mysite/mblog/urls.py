589965e122d27b9226855f0b4d3d82f292163df2
Rashid <rassh1512@gmail.com>
2022-02-11 15:37:36 +0530

    Migrate from view function to classview function in blogsite

--- Django/mysite/mblog/urls.py
+++ Django/mysite/mblog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="starting"),
-    path('posts',views.posts, name = 'post'),
-    path('post/<slug:slug>', views.post_detail, name= 'post-detail')#posts/my-first-post
+    path('',views.StartingPageView.as_view(), name="starting"),
+    path('posts',views.AllPostView.as_view(), name = 'post'),
+    path('post/<slug:slug>', views.PostDetailView.as_view(), name= 'post-detail')#posts/my-first-post
 ]
--- Django/mysite/mblog/views.py
+++ Django/mysite/mblog/views.py
@@ -1,8 +1,43 @@
+from re import template
 from django.shortcuts import render , get_object_or_404
+from matplotlib.style import context
 from .models import Post
-
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+class StartingPageView(ListView):
+    template_name = "index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        data = queryset[:3]
+        return data
+    
+
+class AllPostView(ListView):
+    template_name = "all-post.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_post"
+
+    
+class PostDetailView(DetailView):
+    template_name = "post-details.html"
+    model = Post
+    context_object_name = "identified_post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+    
+
+'''
+
 def index(request):
     latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, "index.html", {"posts": latest_post})
@@ -16,3 +51,5 @@ def post_detail(request,slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "post-details.html",{'identified_post': identified_post,
     'post_tags': identified_post.tags.all()})
+
+    '''
